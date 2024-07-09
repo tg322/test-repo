32e7fac8bed77736af29369c4c539491b45df0a0
tg322 <tom.shaun.grennan@gmail.com>
2024-07-09 13:22:06 +0100

    checking cron on dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          test repo devs
+          test repo devs, second commit, checking cron job
         </p>
         <a
           className="App-link"
