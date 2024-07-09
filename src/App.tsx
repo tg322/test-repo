9bc8c6e2404b2b6a1473cc3f56cc0dfcce05aa0f
tg322 <tom.shaun.grennan@gmail.com>
2024-07-09 15:42:21 +0100

    testing new deploy.sh file

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           test repo devs, second commit, checking cron job
+          checking new deploy.sh file
         </p>
         <a
           className="App-link"
