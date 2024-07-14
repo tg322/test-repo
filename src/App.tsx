30c22c9a2155f2db0861aff7e8ae775ac16ffcc1
tg322 <tom.shaun.grennan@gmail.com>
2024-07-14 13:10:54 +0100

    test 6

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         </p>
         <p>A live website test on dev branch. :</p>
         <p>Automated change test.</p>
-        <p>Automated change test 5</p>
+        <p>Automated change test 6</p>
         <a
           className="App-link"
           href="https://reactjs.org"
